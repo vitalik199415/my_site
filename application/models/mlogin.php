17356d37a392cc2a4ae5f529649eeddafb28c072
VitaliyD <vitalik1994.15@gmail.com>
2014-12-09 22:16:05 +0200

    h

---
+++ application/models/mlogin.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mlogin extends CI_Model {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function login()
+    {
+        
+    }
+
+}
